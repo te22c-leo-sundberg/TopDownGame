d552ffe4c005e1c70028653c3ebf50be90df0ab7
Leo Sundberg <leo.sundberg@elev.ga.ntig.se>
2023-11-13 09:10:38 +0100

    Cimmitfufa

--- TopDownGam/Program.cs
+++ TopDownGam/Program.cs
@@ -2,13 +2,13 @@
 using System.Numerics;
 Random generator = new Random();
 
-Raylib.InitWindow(800, 800, "(‿ˠ‿)");
+Raylib.InitWindow(1600, 900, "(‿ˠ‿)");
 Raylib.SetTargetFPS(60);
 
 float speed = 5;
 
-Texture2D playerRectImage = Raylib.LoadTexture("cryingchild.png");
-Vector2 playerRect = new Vector2(41, 50);
+Texture2D playerRectImage = Raylib.LoadTexture("cryingchild3.png");
+Vector2 playerRect = new Vector2(53, 65);
 Vector2 movement = Vector2.Zero;
 
 while (!Raylib.WindowShouldClose())
@@ -44,9 +44,30 @@ while (!Raylib.WindowShouldClose())
     playerRect.X += movement.X;
     playerRect.Y += movement.Y;
 
+    if (playerRect.X < 0)
+    {
+        playerRect.X = 0;
+    }
+    else if (playerRect.X > 759)
+    {
+        playerRect.X = 759;
+    }
+
+    if (playerRect.Y < 0)
+    {
+        playerRect.Y = 0;
+    }
+    else if (playerRect.Y > 750)
+    {
+        playerRect.Y = 750;
+    }
+
+    //if playerRect.X/.Y = wall.X/.Y speed = 0.
 
     Raylib.BeginDrawing();
-    Raylib.ClearBackground(Color.VIOLET);
+
+    Raylib.ClearBackground(Color.BLACK);
     Raylib.DrawTexture(playerRectImage,(int)playerRect.X, (int)playerRect.Y, Color.WHITE);
+
     Raylib.EndDrawing();
 }
